e500c84baaaa379e6743b24ee91e6362ceb9b70b
Polina Kirichenko <polina.kirichenko@jetbrains.com>
2026-04-30 17:45:18 +0300

    Edit comments at L02/T07

--- L02/T07/ExerciseListSlicing.py
+++ L02/T07/ExerciseListSlicing.py
@@ -11,13 +11,13 @@ answer_3 = letters[1:3]  # Slice 1..3
 
 # List modification
 letters[0] = 'z'
-answer_4 = ['z', 'b', 'c'] # Predict numbers list after the change
+answer_4 = ['z', 'b', 'c'] # Predict letters list after the change
 
 # List operations
 bonus = letters + ['e']
 letters[0] = 'x'
 
-answer_5 = ['x', 'b', 'c'] # Predict numbers now
+answer_5 = ['x', 'b', 'c'] # Predict letters now
 answer_6 = ['z', 'b', 'c', 'e']  # Predict bonus now
 
 
